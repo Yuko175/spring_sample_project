6d051ef7b78e31a37b73e210e7402647e20074c6
Yuko175 <happyyuko175@gmail.com>
2024-02-17 04:44:03 +0900

    追加：バリテーションメッセージが出るようにした

--- sourcecode/web/src/main/java/com/example/web/sample/form/ScForm.java
+++ sourcecode/web/src/main/java/com/example/web/sample/form/ScForm.java
@@ -2,19 +2,19 @@ package com.example.web.sample.form;
 
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
+import nablarch.core.validation.ee.NumberRange;
+
 import java.io.Serializable;
 
 public class ScForm implements Serializable {
 
-  //TODO:数字じゃない文字列の時、バリテーションエラー出ない
-  //TODO:相関バリテーションのエラーメッセージが出ない
   //平成31秋、令和元年春、令和2年春がない
-
   @NotNull(message = "{jakarta.validation.constraints.NotNull}")
   private String genngou;
 
   @NotNull(message = "{jakarta.validation.constraints.NotNull}")
-  private String year;
+  @NumberRange(min = 1, max = 31, message = "有効な数字を入れてください。")
+  private Integer year;
 
   @NotNull(message = "{jakarta.validation.constraints.NotNull}")
   private String season;
@@ -27,11 +27,11 @@ public class ScForm implements Serializable {
     this.genngou = genngou;
   }
 
-  public String getYear() {
+  public Integer getYear() {
     return year;
   }
 
-  public void setYear(String year) {
+  public void setYear(Integer year) {
     this.year = year;
   }
 
@@ -43,45 +43,49 @@ public class ScForm implements Serializable {
     this.season = season;
   }
 
-  //TODO:冗長？
 
   // 相関バリテーション(令和)
   @AssertTrue(message = "開催されていません。")
-  private boolean isValidReiwa() {
-    //令和である
-    if ("令和".equals(genngou)) {
-      int yearValue = Integer.parseInt(year);
-      //1~5である
-      if (yearValue >= 1 && yearValue <= 5) {
-        //1,2年春はない
-        if (yearValue == 1 && "春期".equals(season) || yearValue == 2 && "春期".equals(season)) {
-          return false;
-        }
-        return true;
-      }
+  public boolean isValidReiwa() {
+    //値がnullかどうかの確認
+    if (genngou == null || year == null || season == null) {             
+      return true;                                                        
+    }
+    //令和でない時
+    if (!"令和".equals(genngou)) {
+      return true;
+    }
+    //~0年と6年~はない
+    if (year < 1 || year > 5) {
+      return false;
+    }
+    //令和1年春と令和2年春はない
+    if (year == 1 && "春期".equals(season) || year == 2 && "春期".equals(season)) {
       return false;
     }
     return true;
   }
-  
-
 
   // 相関バリテーション(平成)
   @AssertTrue(message = "開催されていません。")
-  private boolean isValidHeisei() {
-    //平成である
-    if ("平成".equals(genngou)) {
-      int yearValue = Integer.parseInt(year);
-      //21~31である
-      if (yearValue >= 21 && yearValue <= 31) {
-        //31年秋はない
-        if (yearValue==31 && "秋期".equals(season)) {
-          return false;
-        }
-        return true;
-      }
+  public boolean isValidHeisei() {
+    //値がnullかどうかの確認
+    if (genngou == null || year == null || season == null) {            
+      return true;                                                          
+    }
+    //平成でない時
+    if (!"平成".equals(genngou)) {
+      return true;
+    }
+    //~20年と32年~はない    
+    if (year < 21 || year > 31) {
       return false;
     }
+    //平成31年秋がない
+    if (year == 31 && "秋期".equals(season)) {
+      return false;
+    }
+    
     return true;
   }
 }
--- sourcecode/web/src/main/java/com/example/web/sample/service/ScService.java
+++ sourcecode/web/src/main/java/com/example/web/sample/service/ScService.java
@@ -10,49 +10,42 @@ import org.springframework.stereotype.Service;
 public class ScService {
     public ScDto calcSc (
         String genngou,
-        String year,
+        Integer year,
         String season) {
     
     //問題リンク
-    Integer intYear=Integer.parseInt(year);
-    String formatYear = String.format("%02d", intYear);
-
-    String gogo1_1 = "";
+    String formatYear = String.format("%02d", year);
+    String seasonStr="";
+    if (season.equals("春期")) {
+      seasonStr = "haru";
+    } else if(season.equals("秋期"))   {
+      seasonStr = "aki";
+    }
+  
+    String gogoUrl = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr;
+    String gogo1_1 = ""; 
     String gogo1_2 = "";
     String gogo1_3 = "";
     String gogo2_1 = "";
     String gogo2_2 = "";
-
-    String seasonStr = "";
-    if (season.equals("春期")) {
-      seasonStr = "haru";
-
-    } else if(season.equals("秋期"))   {
-      seasonStr = "aki";
-    }
-
-    //TODO:冗長？
-
     if (genngou.equals("令和") && formatYear.equals("05") && season.equals("秋期")) {
       //令和5年度用
-      gogo1_1 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm01.pdf";
-      gogo1_2 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm02.pdf";
-      gogo1_3 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm03.pdf";
-      gogo2_1 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm04.pdf";
-      gogo2_2 = "";
-
+      gogo1_1 += gogoUrl + "/pm01.pdf";
+      gogo1_2 += gogoUrl + "/pm02.pdf";
+      gogo1_3 += gogoUrl + "/pm03.pdf";
+      gogo2_1 += gogoUrl + "/pm04.pdf";
     } else {
       //その他
-      gogo1_1 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm1_1.pdf";
-      gogo1_2 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm1_2.pdf";
-      gogo1_3 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm1_3.pdf";
-      gogo2_1 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm2_1.pdf";
-      gogo2_2 = "https://www.sc-siken.com/pdf/" + formatYear + "_" + seasonStr + "/pm2_2.pdf";
+      gogo1_1 = gogoUrl + "/pm1_1.pdf";
+      gogo1_2 = gogoUrl + "/pm1_2.pdf";
+      gogo1_3 = gogoUrl + "/pm1_3.pdf";
+      gogo2_1 = gogoUrl + "/pm2_1.pdf";
+      gogo2_2 = gogoUrl + "/pm2_2.pdf";
     }
     
     //採点リンク
     BigDecimal adYear = BigDecimal.ZERO;
-    String genngouStr = "";
+    String genngouStr="";
       //令和の場合
     if (genngou.equals("令和")) {
         genngouStr = "r";
@@ -68,7 +61,7 @@ public class ScService {
     String saitenn = "";
       //令和元年秋の場合
     if (genngou.equals("令和")  && formatYear.equals("01")&& season.equals("秋期")) {
-        saitenn="https://www.ipa.go.jp/shiken/mondai-kaiotu/"+adYear+"h31.html#"+seasonStr+"_sc";
+        saitenn="https://www.ipa.go.jp/shiken/mondai-kaiotu/"+adYear+"h31.html#haru_sc";
       } else {
       //その他
         saitenn="https://www.ipa.go.jp/shiken/mondai-kaiotu/"+adYear+genngouStr+formatYear+".html#"+seasonStr+"_sc";
